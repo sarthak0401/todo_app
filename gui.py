682dab83a7745ea7349fed3d1723fb3188873165
sarthak0401 <sarthakbokade413@gmail.com>
2024-11-27 16:53:37 +0530

    removed files directory

--- gui.py
+++ gui.py
@@ -2,9 +2,21 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in the todo")
-input_box  = sg.InputText(tooltip="Enter todo")
+input_box  = sg.InputText(tooltip="Enter todo", key='todo')
 add_button = sg.Button("Add")
 
 window = sg.Window("My todo-app", layout=[[label],[input_box,add_button]])
-window.read()
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    if event == "Add":
+        todos = functions.get_todos()
+        todos.append(values['todo'] + '\n')
+        functions.write_todo(todos)
+    
+    elif event == sg.WIN_CLOSED:
+        break
+
 window.close()
